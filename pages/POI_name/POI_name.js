1f9d8bd40c9f45745d1bede8feeb89ffcd8d244b
WXNwxn <776339794@qq.com>
2018-10-07 10:44:50 +0800

    新建

--- pages/POI_name/POI_name.js
+++ pages/POI_name/POI_name.js
@@ -5,9 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    date: "2016-09-02",
+    time: "12:01",
+    day: '',
+    countryCodes: ["+86", "+80", "+84", "+87"],
+    countryCodeIndex: 0,
   },
-
+});
   /**
    * 生命周期函数--监听页面加载
    */
@@ -62,5 +66,42 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  bindDateChange: function (e) {
+    let Day = new Date(e.detail.value).getDay()
+    switch (Day) {
+      case 1:
+        Day = "一"
+        break;
+      case 2:
+        Day = "二"
+        break;
+      case 3:
+        Day = "三"
+        break;
+      case 4:
+        Day = "四"
+        break;
+      case 5:
+        Day = "五"
+        break;
+      case 6:
+        Day = "六"
+        break;
+      case 7:
+        Day = "七"
+        break;
+      default:
+        Day = ""
+    }
+    this.setData({
+      date: e.detail.value,
+      day: Day
+    })
+  },
+  bindTimeChange: function (e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
 })
