3014129f49b7f9c70711be5b7f597bd5c3fbc600
minchang0227 <503240154@qq.com>
2018-10-08 13:07:49 +0800

    进入地图

--- pages/enter_map/enter_map.js
+++ pages/enter_map/enter_map.js
@@ -1,11 +1,34 @@
-// pages/select_cover/select_cover.js
+// pages/enter_map/enter_map.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-   
+    inputShowed: false,
+    inputVal: ""
+
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
   },
 
   /**
@@ -63,5 +86,4 @@ Page({
   onShareAppMessage: function () {
 
   }
-
 })
