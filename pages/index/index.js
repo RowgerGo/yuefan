233d88701e25954eab294292840c766e015a427e
WXNwxn <776339794@qq.com>
2018-10-09 10:03:38 +0800

    poi修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,139 +5,29 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    grids:[0,0,2,3,34,435,345,456,45645],
+    grids: [0, 0, 2, 3, 34, 435, 345, 456, 45645],
     userInfo: {},
-    grid_select:8,
+    grid_select: 8,
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     showTopTips: false,
 
-    radioItems: [
-      { name: 'cell standard', value: '0' },
-      { name: 'cell standard', value: '1', checked: true }
-    ],
-    checkboxItems: [
-      { name: 'standard is dealt for u.', value: '0', checked: true },
-      { name: 'standard is dealicient for u.', value: '1' }
-    ],
-
     date: "2016-09-02",
     time: "12:01",
-    day:'',
+    day: '',
     countryCodes: ["+86", "+80", "+84", "+87"],
     countryCodeIndex: 0,
-
-    countries: ["中国", "美国", "英国"],
-    countryIndex: 0,
-
-    accounts: ["微信号", "QQ", "Email"],
-    accountIndex: 0,
 
     isAgree: false
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function (option) {
-    let a="罗杰" 
-    
-    let type = option.type
-    let value =option.value
-    
-   
-
-    wx.showToast({
-      title: type + value,
-      icon: 'success',
-      duration: 2000
-    })
 
 
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-  showTopTips: function () {
-    var that = this;
-    this.setData({
-      showTopTips: true
-    });
-    setTimeout(function () {
-      that.setData({
-        showTopTips: false
-      });
-    }, 3000);
-  },
-  radioChange: function (e) {
-    console.log('radio发生change事件，携带value值为：', e.detail.value);
-
-    var radioItems = this.data.radioItems;
-    for (var i = 0, len = radioItems.length; i < len; ++i) {
-      radioItems[i].checked = radioItems[i].value == e.detail.value;
-    }
-
-    this.setData({
-      radioItems: radioItems
-    });
-  },
-  checkboxChange: function (e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
-
-    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
-    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-      checkboxItems[i].checked = false;
-
-      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-        if (checkboxItems[i].value == values[j]) {
-          checkboxItems[i].checked = true;
-          break;
-        }
-      }
-    }
-
-    this.setData({
-      checkboxItems: checkboxItems
-    });
-  },
-  bindDateChange: function (e) {
+  bindDateChange: function(e) {
     let Day = new Date(e.detail.value).getDay()
     switch (Day) {
       case 1:
-        Day="一"
+        Day = "一"
         break;
       case 2:
         Day = "二"
@@ -158,50 +48,17 @@ Page({
         Day = "七"
         break;
       default:
-        Day=""
+        Day = ""
     }
     this.setData({
       date: e.detail.value,
-      day:Day
+      day: Day
     })
   },
-  bindTimeChange: function (e) {
+  bindTimeChange: function(e) {
     this.setData({
       time: e.detail.value
     })
-  },
-  bindCountryCodeChange: function (e) {
-    console.log('picker country code 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      countryCodeIndex: e.detail.value
-    })
-  },
-  bindCountryChange: function (e) {
-    console.log('picker country 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      countryIndex: e.detail.value
-    })
-  },
-  bindAccountChange: function (e) {
-    console.log('picker account 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      accountIndex: e.detail.value
-    })
-  },
-  bindAgreeChange: function (e) {
-    this.setData({
-      isAgree: !!e.detail.value.length
-    });
-  },
-  grid_select:function(e){
-    var index = e.currentTarget.dataset.index;
-
-    console.log(index)
-    this.setData({
-      grid_select:index
-    })
   }
+
 })
