66ecef90fecdd5eb08bcdd2befe13bce02b406f8
六分 <php_echo@163.com>
2018-10-16 18:14:20 +0800

    首页我的饭局数据

--- components/game_tool_header/game_tool_header.js
+++ components/game_tool_header/game_tool_header.js
@@ -1,4 +1,5 @@
 // components/game_tool_header/game_tool_header.js
+const app=getApp()
 Component({
   /**
    * 组件的属性列表
@@ -18,13 +19,18 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    getUserInfoFun:function(){
-      wx.navigateTo({
-        url: "../../pages/select_time/select_time"
-      })
+    getUserInfoFun:function(e){
+      console.log(e.detail)
+      if (e.detail.errMsg == "getUserInfo:ok"){
+        getApp().globalData.nickname = e.detail.userInfo.nickName
+        wx.navigateTo({
+          url: "../../pages/select_time/select_time"
+        })
+      }
+      
+   
 
-
-      console.log(1233)
+     
     }
   },
  
