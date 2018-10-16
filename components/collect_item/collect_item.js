1aa77a0fd5d235853be980a7fe29f0244705c820
六分 <php_echo@163.com>
2018-10-17 00:00:32 +0800

    首页列表样式 调整

--- components/collect_item/collect_item.js
+++ components/collect_item/collect_item.js
@@ -11,6 +11,10 @@ Component({
     type: {            // 属性名
       type: Number,     // 类型（必填），目前接受的类型包括：String, Number, Boolean, Object, Array, null（表示任意类型）
       value: ''     // 属性初始值（可选），如果未指定则会根据类型选择一个
+    },
+    index:{
+      type: Number,    
+      value: ''    
     }
   },
 
